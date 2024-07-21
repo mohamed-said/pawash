b98af62044fce00fc0b34af898ce44e7efe8a8f3
Mohamed Said <mohamed.said.fci@gmail.com>
2024-07-22 00:10:10 +0200

    refactor download subcommand

--- src/subcommands/download.rs
+++ src/subcommands/download.rs
@@ -5,7 +5,7 @@
 use anyhow::{Context, Result};
 use futures_util::StreamExt;
 use indicatif::{ProgressBar, ProgressStyle};
-use reqwest::Client;
+use reqwest::{Client, Response};
 use std::cmp::min;
 use std::fs::File;
 use std::io::Write;
@@ -20,20 +20,7 @@ impl Download {
         Self { client }
     }
 
-    pub async fn download_file(&self, url: String, path: String) -> Result<()> {
-        // Reqwest setup
-        let res = self
-            .client
-            .get(url.clone())
-            .send()
-            .await
-            .context(format!("Failed to GET from '{}'", url.clone()))?;
-
-        let total_size = res.content_length().context(format!(
-            "Failed to get content length from '{}'",
-            url.clone()
-        ))?;
-
+    fn build_progress_bar(&self, total_size: u64) -> Result<ProgressBar> {
         // Indicatif setup
         // move progress bar as a struct member field
         let pb = ProgressBar::new(total_size);
@@ -52,29 +39,51 @@ impl Download {
                 .progress_chars("||."),
         );
 
-        pb.set_message(format!("Downloading {}", url.clone()));
+        Ok(pb)
+    }
+    async fn get_file(&self, url: &str) -> Result<Response> {
+        // Reqwest setup
+        self.client
+            .get(url)
+            .send()
+            .await
+            .context(format!("Failed to GET from '{}'", url))
+    }
+
+    pub async fn download_file(&self, url: String, path: String) -> Result<()> {
+        let response = self.get_file(url.as_str()).await?;
+
+        let total_size = response.content_length().context(format!(
+            "Failed to get content length from '{}'",
+            url.clone()
+        ))?;
+
+        // create progress bar
+        let progress_bar = self.build_progress_bar(total_size)?;
+        progress_bar.set_message(format!("Downloading {}", url.clone()));
 
         // download chunks
         let mut file = File::create(path.clone())
             .context(format!("Failed to create file '{}'", path.clone()))?;
 
         let mut downloaded: u64 = 0;
-        let mut stream = res.bytes_stream();
+        let mut stream = response.bytes_stream();
 
         while let Some(item) = stream.next().await {
             // TODO handle this properly in a match case
-            let chunk = item.context(format!("Error while downloading file"))?;
+            let chunk = item.context("Error while downloading file".to_string())?;
 
             // TODO specify the chunk number in the error message
             file.write_all(&chunk)
-                .context(format!("Error while writing to file"))?;
+                .context("Error while writing to file".to_string())?;
 
             let new = min(downloaded + (chunk.len() as u64), total_size);
             downloaded = new;
-            pb.set_position(new);
+            progress_bar.set_position(new);
         }
 
-        pb.finish_with_message(format!("Downloaded {} to {}", url.clone(), path.clone()));
-        return Ok(());
+        progress_bar.finish_with_message(format!("Downloaded {} to {}", url.clone(), path.clone()));
+
+        Ok(())
     }
 }
